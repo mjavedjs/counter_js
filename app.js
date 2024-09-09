be34627bb89cb1d25df3beaf125c74a12dcc82b8
muhammadjaved238 <mjavedjs2006@gmail.com>
2024-09-10 02:25:27 +0500

    counter

---
+++ app.js
@@ -0,0 +1,10 @@
+let heading=document.querySelector("#heading");
+function inc(){
+    heading.innerHTML=++heading.innerHTML
+}
+function dec(){
+    heading.innerHTML=--heading.innerHTML
+}
+function rest(){
+    heading.innerHTML="0"
+}
